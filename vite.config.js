76b397a97f366efd2778a6a2701a14f6f9dd1208
LVCC Kristel Magpayo <magpayok707@gmail.com>
2024-11-03 12:33:25 +0800

    Apply stash changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/personal-portfolio/",
+  base: "/personal-portfolio/",  // Keeps your base URL for deployment
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Splitting third-party libraries into separate chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,  // Adjust the size limit for chunk warnings (1000 KB = 1 MB)
+  },
 })
